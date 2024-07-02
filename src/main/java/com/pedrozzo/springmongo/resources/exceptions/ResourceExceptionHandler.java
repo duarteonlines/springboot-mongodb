43b4aae58503be0b21635b80ad5da27628eafc32
pedrozzoo <p.duarteoliveiraa@gmail.com>
2024-07-02 16:39:37 -0300

    Obtendo um usuario por ID

---
+++ src/main/java/com/pedrozzo/springmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.pedrozzo.springmongo.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.pedrozzo.springmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado!", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
