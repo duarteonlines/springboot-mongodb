d2a6cc626fdca5839ca42b3bfdf45ffdaa2471e2
pedrozzoo <p.duarteoliveiraa@gmail.com>
2024-07-02 19:45:25 -0300

    Atualizacao de Usuario com PUT

--- src/main/java/com/pedrozzo/springmongo/services/UserService.java
+++ src/main/java/com/pedrozzo/springmongo/services/UserService.java
@@ -38,4 +38,16 @@ public class UserService {
 		findById(id);
 		repository.deleteById(id);
 	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	 
 }
