29fd1f561dcbf791f434046f9c25179a47c63199
pedrozzoo <p.duarteoliveiraa@gmail.com>
2024-07-03 18:10:20 -0300

    Consulta simples com @Query

--- src/main/java/com/pedrozzo/springmongo/repository/PostRepository.java
+++ src/main/java/com/pedrozzo/springmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.pedrozzo.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedrozzo.springmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.pedrozzo.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title' : {$regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
