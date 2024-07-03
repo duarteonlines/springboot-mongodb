cb83f258edb3af618935c1f94cc894ac7bfcc4f7
pedrozzoo <p.duarteoliveiraa@gmail.com>
2024-07-03 15:21:35 -0300

    Referenciando os Posts do Usuario

--- src/main/java/com/pedrozzo/springmongo/config/Instantiation.java
+++ src/main/java/com/pedrozzo/springmongo/config/Instantiation.java
@@ -43,6 +43,10 @@ public class Instantiation implements CommandLineRunner{
 		
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		
+		userRepository.save(maria);
 	}
 
 }
